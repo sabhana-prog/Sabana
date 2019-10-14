a225036a455f51ccf74abd96fd3e33c9dc85a702
tiger <sathish@gmail.com>
2019-10-14 10:58:32 +0530

    File added

---
+++ swap.py
@@ -0,0 +1,7 @@
+a=int(input('Enter the first value'))
+b=int(input('Enter the second value'))
+print("Your first value is",a,"and Your second value is",b)
+temp=a
+a=b
+b=temp
+print("After swapping Your first value is",a,"and Your second value is",b)
